96cff2f18e55f984bd8e088304bfcbe7b8fb270e
JaguarMantid <jaguarmantid@gmail.com>
2021-01-25 18:19:16 +0000

    Part 10

--- engine.py
+++ engine.py
@@ -1,10 +1,11 @@
 from __future__ import annotations
+import lzma
+import pickle
 from tcod.console import Console
 from tcod.map import compute_fov
 from typing import TYPE_CHECKING
 
 import exceptions
-from input_handlers import MainGameEventHandler
 from message_log import MessageLog
 from render_functions import render_bar, render_names_at_mouse_location
 from tile_types import TA_TRANSPARENT
@@ -12,14 +13,12 @@ from tile_types import TA_TRANSPARENT
 if TYPE_CHECKING:
     from entity import Actor
     from game_map import GameMap
-    from input_handlers import EventHandler
 
 
 class Engine:
     game_map: GameMap
     
     def __init__(self, player: Actor):
-        self.event_handler: EventHandler = MainGameEventHandler(self)
         self.message_log = MessageLog()
         self.mouse_location = (0,0)
         self.player = player
@@ -31,16 +30,6 @@ class Engine:
                     entity.ai.perform()
                 except exceptions.Impossible:
                     pass  # Ignore impossible action exceptions from AI.
-
-    def update_fov(self) -> None:
-        """Recompute the visible area based on the players point of view."""
-        self.game_map.visible[:] = compute_fov(
-            self.game_map.tiles[TA_TRANSPARENT],
-            (self.player.x, self.player.y),
-            radius=8,
-        )
-        # If a tile is "visible" it should be added to "explored".
-        self.game_map.explored |= self.game_map.visible
 
     def render(self, console: Console) -> None:
         self.game_map.render(console)
@@ -55,3 +44,19 @@ class Engine:
         )
 
         render_names_at_mouse_location(console=console, x=21, y=44, engine=self)
+
+    def save_as(self, filename: str) -> None:
+        """Save this Engine instance as a compressed file."""
+        save_data = lzma.compress(pickle.dumps(self))
+        with open(filename, "wb") as f:
+            f.write(save_data)
+
+    def update_fov(self) -> None:
+        """Recompute the visible area based on the players point of view."""
+        self.game_map.visible[:] = compute_fov(
+            self.game_map.tiles[TA_TRANSPARENT],
+            (self.player.x, self.player.y),
+            radius=8,
+        )
+        # If a tile is "visible" it should be added to "explored".
+        self.game_map.explored |= self.game_map.visible
